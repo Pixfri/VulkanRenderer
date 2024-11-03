885072663bccfa511e7a02eadf4174649ef4dac0
Jean <jean.letessier@protonmail.com>
2024-11-03 18:17:58 +0100

    Project setup*.

---
+++ Source/VulkanRenderer/Main.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2024 Jean "Pixfri" Letessier 
+// This file is part of Vulkan Renderer.
+// For conditions of distribution and use, see copyright notice in LICENSE.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,16 @@
 set_xmakever("2.9.3")
 
-set_project("ProjectName")
+set_project("VulkanRenderer")
 
 add_rules("mode.debug", "mode.release")
 set_languages("cxx20")
 
+includes("xmake/**.lua")
+
 option("override_runtime", {description = "Override VS runtime to MD in release and MDd in debug.", default = true})
+option("libx11", { description = "Enable LibX11 support (requires the libx11 package).", default = true })
+option("libxcb", { description = "Enable LibXCB support (requires the libxcb package).", default = false })
+option("wayland", { description = "Enable Wayland support (requires the wayland package).", default = false })
 
 add_includedirs("Include")
 
@@ -16,7 +21,7 @@ if is_mode("release") then
   set_optimize("fastest")
   set_symbols("debug", "hidden")
 else
-  add_defines("PN_DEBUG")
+  add_defines("VKR_DEBUG")
 end
 
 set_encodings("utf-8")
@@ -32,9 +37,34 @@ if is_plat("windows") then
   end
 end
 
+if is_plat("windows", "mingw") then
+  add_syslinks("user32", "kernel32")
+  add_defines("VK_USE_PLATFORM_WIN32_KHR")
+elseif is_plat("linux") then
+  add_syslinks("dl")
+  if has_config("libx11") then
+    add_defines("VK_USE_PLATFORM_XLIB_KHR")
+    add_requires("libx11")
+  end
+
+  if has_config("libxcb") then
+    add_defines("VK_USE_PLATFORM_XCB_KHR")
+    add_requires("libxcb")
+  end
+
+  if has_config("wayland") then
+    add_defines("VK_USE_PLATFORM_WAYLAND_KHR")
+    add_requires("wayland")
+  end
+end
+
+add_defines("VK_NO_PROTOTYPES")
+
 add_cxflags("-Wno-missing-field-initializers -Werror=vla", {tools = {"clang", "gcc"}})
 
-target("ProjectName")
+add_requires("vulkan-headers")
+
+target("VulkanRenderer")
   set_kind("binary")
   
   add_files("Source/**.cpp")
@@ -44,3 +74,22 @@ target("ProjectName")
   end
   
   add_rpathdirs("$ORIGIN")
+
+  if is_plat("linux") then
+    if has_config("libx11") then
+      add_defines("VK_USE_PLATFORM_XLIB_KHR")
+      add_requires("libx11")
+    end
+
+    if has_config("libxcb") then
+      add_defines("VK_USE_PLATFORM_XCB_KHR")
+      add_requires("libxcb")
+    end
+
+    if has_config("wayland") then
+      add_defines("VK_USE_PLATFORM_WAYLAND_KHR")
+      add_requires("wayland")
+    end
+  end
+
+  add_packages("vulkan-headers")
